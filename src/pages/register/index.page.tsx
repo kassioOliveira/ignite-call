81675b6a33e5c7d0b61de35b5b5baf704ddf3459
kassio oliveira <kassiooliveira875@gmail.com>
2023-05-25 15:41:14 -0300

    Página de conexão do calendário

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -51,6 +51,8 @@ export default function Register() {
         name,
         username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (error instanceof AxiosError && error?.response?.data?.message) {
         alert(error.response.data.message)
