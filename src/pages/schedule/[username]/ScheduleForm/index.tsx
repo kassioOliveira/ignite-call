ed09aeba99ee32ea48a7b7210fd1805f7931c60f
kassio oliveira <kassiooliveira875@gmail.com>
2023-06-09 09:48:53 -0300

    Formulário de confirmação

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,7 +1,5 @@
-// import { CalendarStep } from './CalendarStep'
 import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
   return <ConfirmStep />
-  // return <CalendarStep />
 }
